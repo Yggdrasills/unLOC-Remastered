c08f50c7269a91b08985f58937787f86ce8e498a
Stanislav Kuznichenko <stanislav.k@amayasoft.uz>
2022-05-02 13:28:35 +0300

    Reconfigure base moving mechanic..

--- Assets/Scripts/Core/Moving/Demo/DemoPlayerView.cs
+++ Assets/Scripts/Core/Moving/Demo/DemoPlayerView.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 
 using Cysharp.Threading.Tasks;
@@ -10,37 +11,53 @@ using VContainer.Unity;
 
 namespace SevenDays.unLOC.Core.Moving.Demo
 {
-    public class DemoPlayerView : MonoBehaviour, IMovable, ITickable
+    public class DemoPlayerView : MonoBehaviour, IInitializable, IMovable, ITickable
     {
+        private const int RightSideValue = 1;
+        private const int LeftSideValue = -1;
+        
         [SerializeField] private float _movingDuration = 2;
         [SerializeField] private float _movingSpeed = 2;
+        [SerializeField] private Animator _playerAnimator;
+        [SerializeField] private Vector2 _playerRootMovableRange;
+
         private Tween _activeTween;
+        private List<string> _walkingAnimations;
 
         public bool IsActive { get; set; }
         public bool IsMoving { get; private set; }
-        
+
+        void IInitializable.Initialize()
+        {
+            _walkingAnimations = new List<string>() {"PlayerWalkRight", "PlayerWalkLeft"};
+        }
+
         UniTask IMovable.MoveToPointAsync(Vector3 point, CancellationToken token)
         {
             if (!IsActive) return UniTask.CompletedTask;
 
             MoveHorizontalAsync(point.x).Forget();
-            
+
             return UniTask.CompletedTask;
         }
-        
+
         void ITickable.Tick()
         {
             if (!IsActive) return;
-            
+
             if (IsMoving) return;
-            
+
             var translation = Input.GetAxis("Horizontal") * _movingSpeed;
 
             if (translation.Equals(0)) return;
-            
+
             RotatePlayer(translation, 0);
-                
+
             translation *= Time.deltaTime;
+            
+            if (transform.position.x <= _playerRootMovableRange.x && translation < 0) return;
+            
+            if(transform.position.x >= _playerRootMovableRange.y && translation > 0) return;
 
             transform.Translate(translation, 0, 0);
         }
@@ -56,7 +73,7 @@ namespace SevenDays.unLOC.Core.Moving.Demo
         void IMovable.PauseMoving()
         {
             if (!IsMoving) return;
-            
+
             if (_activeTween.active && _activeTween != null)
             {
                 _activeTween.Pause();
@@ -75,6 +92,11 @@ namespace SevenDays.unLOC.Core.Moving.Demo
         {
             RotatePlayer(horizontalPoint, transform.position.x);
 
+            if (!IsInRange(horizontalPoint, out var inRangePoint))
+            {
+                horizontalPoint = inRangePoint;
+            }
+
             _activeTween = transform.DOMoveX(horizontalPoint, _movingDuration);
 
             IsMoving = true;
@@ -86,8 +108,30 @@ namespace SevenDays.unLOC.Core.Moving.Demo
 
         private void RotatePlayer(float horizontalPoint, float comparableValue)
         {
-            transform.localScale = horizontalPoint < comparableValue ?
-                new Vector3(-1,transform.localScale.y,1) : new Vector3(1,transform.localScale.y,1);
+            var rotationValue = horizontalPoint < comparableValue ? LeftSideValue : RightSideValue;
+            
+            //todo: Reconfigure player animation rotation root to full control rotation state of animations while translating or idle
+                //transform.localScale = new Vector3(rotationValue, transform.localScale.y, 1);
+
+            _playerAnimator.Play(rotationValue.Equals(LeftSideValue) ? _walkingAnimations[1] : _walkingAnimations[0]);
+        }
+
+        private bool IsInRange(float horizontalPoint, out float inRangePoint)
+        {
+            if (horizontalPoint <= _playerRootMovableRange.x)
+            {
+                inRangePoint = _playerRootMovableRange.x;
+                return false;
+            }
+            
+            if (horizontalPoint >= _playerRootMovableRange.y)
+            {
+                inRangePoint = _playerRootMovableRange.y;
+                return false;
+            }
+
+            inRangePoint = horizontalPoint;
+            return true;
         }
     }
 }
