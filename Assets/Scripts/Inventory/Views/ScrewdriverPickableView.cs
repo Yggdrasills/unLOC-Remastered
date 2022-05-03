84a144e07804a3e1109014110a39d7335e284760
Kamalov Khamro <khamrokamalov@gmail.com>
2022-05-03 16:44:55 +0500

    [Flower Quest] Added screwdriver visual logic

--- Assets/Scripts/Inventory/Views/ScrewdriverPickableView.cs
+++ Assets/Scripts/Inventory/Views/ScrewdriverPickableView.cs
@@ -2,15 +2,20 @@
 
 using SevenDays.unLOC.Services;
 
+using UnityEngine;
+
 namespace SevenDays.unLOC.Views
 {
-    public class ScrewdriverView : PickableBaseView
+    public class ScrewdriverPickableView : PickableBaseView
     {
+        [SerializeField]
+        private GameObject _screwdriverContent;
+
         public override InventoryItem Type => InventoryItem.Screwdriver;
 
         public override Action GetInventoryClickStrategy()
         {
-            return base.GetInventoryClickStrategy();
+            return () => _screwdriverContent.gameObject.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/Screwdriver/ScrewdriverPowerButtonView.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SevenDays.unLOC.Screwdriver
+{
+    public class ScrewdriverPowerButtonView : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField]
+        private Transform _target;
+
+        [SerializeField]
+        private float _duration = 0.5f;
+
+        private Tween _scaleTween;
+
+        private bool _isShown;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            DoScale();
+        }
+
+        private void DoScale()
+        {
+            if (_scaleTween != null && _scaleTween.IsActive())
+                _scaleTween.Kill();
+
+            var scale = _isShown ? Vector3.zero : Vector3.one;
+
+            _scaleTween = _target.DOScale(scale, _duration);
+
+            _isShown = !_isShown;
+        }
+    }
+}
