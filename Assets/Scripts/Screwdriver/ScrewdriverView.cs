74151b0593ef4720940221d52bd91beab4e05e63
Stanislav Kuznichenko <stanislav.k@amayasoft.uz>
2022-05-04 21:53:03 +0300

    Added base demo movable hero into workshop scene.

--- Assets/Scripts/Screwdriver/ScrewdriverView.cs
+++ Assets/Scripts/Screwdriver/ScrewdriverView.cs
@@ -66,7 +66,7 @@ namespace SevenDays.unLOC.Screwdriver
 
             _text.text = ActiveNozzle.ToString();
 
-            await _moveTween;
+            await _moveTween.AsyncWaitForCompletion();
         }
 
         [Serializable]
