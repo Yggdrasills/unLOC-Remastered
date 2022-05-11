b56e71952336981c1febc9cb427aa184a592b2cd
Kamalov Khamro <khamrokamalov@gmail.com>
2022-05-11 23:36:38 +0500

    [*] Item interaction bug fixed

--- Assets/Scripts/Activities/Items/ClickableItem.cs
+++ Assets/Scripts/Activities/Items/ClickableItem.cs
@@ -5,12 +5,18 @@ using UnityEngine.EventSystems;
 
 namespace SevenDays.unLOC.Activities.Items
 {
+    [RequireComponent(typeof(Collider2D))]
     public class ClickableItem : MonoBehaviour, IPointerClickHandler
     {
         public event Action Clicked = delegate { };
 
+        public bool CanInteract { protected get; set; } = true;
+
         void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
         {
+            if (!CanInteract)
+                return;
+
             Clicked.Invoke();
         }
     }
