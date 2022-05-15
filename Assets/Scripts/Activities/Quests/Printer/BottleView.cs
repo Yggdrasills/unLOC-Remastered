10783a752d45cf4d257d74940e767e72e6b2a1a6
Kamalov Khamro <khamrokamalov@gmail.com>
2022-05-16 01:46:01 +0500

    [Printer Quest] Added printer quest logic

---
+++ Assets/Scripts/Activities/Quests/Printer/BottleView.cs
@@ -0,0 +1,51 @@
+﻿using Cysharp.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+using SevenDays.unLOC.Activities.Items;
+using SevenDays.unLOC.Inventory;
+using SevenDays.unLOC.Inventory.Services;
+using SevenDays.unLOC.Inventory.Views;
+
+using UnityEngine;
+
+using VContainer;
+
+namespace SevenDays.unLOC.Activities.Quests.Printer
+{
+    public class BottleView : PickableBase
+    {
+        [SerializeField]
+        private InteractableItem _clickableItem;
+
+        public override InventoryItem Type => InventoryItem.Bottle;
+
+        private IInventoryService _inventory;
+
+        [Inject, UsedImplicitly]
+        private void Construct(IInventoryService inventory)
+        {
+            _inventory = inventory;
+        }
+
+        private void OnValidate()
+        {
+            _clickableItem ??= GetComponent<InteractableItem>();
+        }
+
+        private void OnEnable()
+        {
+            _clickableItem.Clicked += OnClick;
+        }
+
+        private void OnDisable()
+        {
+            _clickableItem.Clicked -= OnClick;
+        }
+
+        private void OnClick()
+        {
+            _inventory.AddAsync(this).Forget();
+        }
+    }
+}
---
+++ Assets/Scripts/Activities/Quests/Printer/PrinterQuest.cs
@@ -0,0 +1,61 @@
+﻿using JetBrains.Annotations;
+
+using SevenDays.unLOC.Activities.Items;
+using SevenDays.unLOC.Inventory;
+using SevenDays.unLOC.Inventory.Services;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+using VContainer;
+
+namespace SevenDays.unLOC.Activities.Quests.Printer
+{
+    public class PrinterQuest : QuestBase, IPointerClickHandler
+    {
+        [SerializeField]
+        private WiresView _wiresPrefab;
+
+        [SerializeField]
+        private InteractableItem _printerView;
+
+        [SerializeField]
+        private GameObject _container;
+
+        [SerializeField]
+        private int _bottlesAmount = 5;
+
+        private IInventoryService _inventory;
+
+        private int _droppedBottlesAmount;
+
+        [Inject, UsedImplicitly]
+        private void Construct(IInventoryService inventory)
+        {
+            _inventory = inventory;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (!_inventory.Contains(InventoryItem.Bottle))
+                return;
+
+            _inventory.Use(InventoryItem.Bottle);
+
+            _droppedBottlesAmount++;
+
+            if (_droppedBottlesAmount < _bottlesAmount)
+                return;
+
+            var wires = Instantiate(_wiresPrefab);
+
+            _inventory.AddAsync(wires);
+
+            _printerView.Disable();
+
+            _container.SetActive(false);
+
+            CompleteQuest();
+        }
+    }
+}
