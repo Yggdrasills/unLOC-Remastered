32b7ad85f130b5c308acfbc746fbe14d6ec8541e
OrlovVasily <orlov.v@amayasoft.uz>
2022-08-14 19:48:21 +0300

    Fix after merge

--- Assets/Scripts/Core/WorkshopCompositeRoot.cs
+++ Assets/Scripts/Core/WorkshopCompositeRoot.cs
@@ -18,35 +18,16 @@ namespace SevenDays.unLOC.Core
     public class WorkshopCompositeRoot : AutoInjectableLifeTimeScope
     {
         [SerializeField]
-        private InventoryCellView _cellPrefab;
-
-        [SerializeField]
-        private InventoryView _inventoryView;
-
-        [SerializeField]
         private Camera _camera;
 
         [SerializeField]
         private InitializeConfig _initializeConfig;
 
-
+        
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.RegisterComponentInHierarchy<AutoInjectableLifeTimeScope>()
-                .AsImplementedInterfaces()
-                .AsSelf();
-
-            RegisterInventory(builder);
             RegisterDialogues(builder);
             RegisterMovement(builder);
-        }
-
-        private void RegisterInventory(IContainerBuilder builder)
-        {
-            builder.Register<InventoryService>(Lifetime.Singleton)
-                .WithParameter(_cellPrefab)
-                .WithParameter(_inventoryView)
-                .AsImplementedInterfaces();
         }
 
         private void RegisterDialogues(IContainerBuilder builder)
