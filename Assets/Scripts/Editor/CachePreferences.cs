e5882c561121c3d4e9ee0048b6c59bccedea44a7
Kamalov Khamro <khamrokamalov@gmail.com>
2022-09-15 23:06:59 +0500

    [*] Remake preferences clearing

--- Assets/Scripts/Editor/CachePreferences.cs
+++ Assets/Scripts/Editor/CachePreferences.cs
@@ -1,15 +1,24 @@
-﻿using ToolBox.Serialization;
+﻿using System.IO;
 
 using UnityEditor;
+
+using UnityEngine;
 
 namespace SevenDays.Utils.Editor
 {
     public static class CachePreferences
     {
-        [MenuItem("Tools/" + nameof(ClearDataSerializer))]
-        public static void ClearDataSerializer()
+        [MenuItem("Tools/" + nameof(ClearCache))]
+        public static void ClearCache()
         {
-            DataSerializer.DeleteAll();
+            string[] files = Directory.GetFiles(Application.persistentDataPath, "*.data");
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                File.Delete(files[i]);
+            }
+
+            PlayerPrefs.DeleteAll();
         }
     }
 }
