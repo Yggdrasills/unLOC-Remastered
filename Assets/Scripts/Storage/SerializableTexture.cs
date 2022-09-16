243b5120b2068936f8a3ea0aa29984556d5dcfe5
Kamalov Khamro <khamrokamalov@gmail.com>
2022-09-16 23:12:53 +0500

    [+] Added Sprite to bytes converter

---
+++ Assets/Scripts/Storage/SerializableTexture.cs
@@ -0,0 +1,11 @@
+﻿namespace SevenDays.unLOC.Storage
+{
+    public class SerializableTexture
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public byte[] Bytes { get; set; }
+    }
+}
---
+++ Assets/Scripts/Storage/SpriteConverter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace SevenDays.unLOC.Storage
+{
+    public static class SpriteConverter
+    {
+        public static SerializableTexture GetTexture(Sprite sprite)
+        {
+            var texture = sprite.texture;
+
+            return new SerializableTexture
+            {
+                Width = texture.width,
+                Height = texture.height,
+                Bytes = texture.EncodeToPNG()
+            };
+        }
+
+        public static Sprite GetSprite(SerializableTexture serializableTexture)
+        {
+            var texture = new Texture2D(serializableTexture.Width, serializableTexture.Height);
+
+            texture.LoadImage(serializableTexture.Bytes);
+
+            return Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), Vector2.one);
+        }
+    }
+}
