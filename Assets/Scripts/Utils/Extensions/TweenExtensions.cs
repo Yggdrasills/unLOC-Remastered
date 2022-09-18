0e33550c42e8d0ac19491aee93562a2da2fa0cb4
Kamalov Khamro <khamrokamalov@gmail.com>
2022-09-18 22:53:30 +0500

    [+] Added tween kill with cts extension

---
+++ Assets/Scripts/Utils/Extensions/TweenExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+using Cysharp.Threading.Tasks;
+
+using DG.Tweening;
+
+namespace SevenDays.unLOC.Utils.Extensions
+{
+    public static class TweenExtensions
+    {
+        public static UniTask AwaitWithCancellation(this Tween tween, CancellationToken cancellationToken)
+        {
+            var registration = cancellationToken.Register(() =>
+            {
+                if (tween.IsActive())
+                {
+                    tween.Kill();
+                }
+            });
+
+            tween.OnKill(() => { registration.Dispose(); });
+
+            return tween.ToUniTask(TweenCancelBehaviour.KillAndCancelAwait, cancellationToken);
+        }
+    }
+}
