42da4e3211d6644b1db84a0c5ca013987313f7f4
Kamalov Khamro <khamrokamalov@gmail.com>
2022-09-27 21:48:06 +0500

    [*] Mechanoid disabling

--- Assets/Scripts/Activities/Quests/Mechanoid/MechanoidView.cs
+++ Assets/Scripts/Activities/Quests/Mechanoid/MechanoidView.cs
@@ -1,5 +1,7 @@
 ﻿using Cysharp.Threading.Tasks;
 
+using SevenDays.unLOC.Activities.Quests.Manager;
+
 using UnityEngine;
 
 namespace SevenDays.unLOC.Activities.Quests.Mechanoid
@@ -7,10 +9,41 @@ namespace SevenDays.unLOC.Activities.Quests.Mechanoid
     public class MechanoidView : MonoBehaviour
     {
         [SerializeField]
+        private GameObject _content;
+
+        [SerializeField]
         private SpriteRenderer _renderer;
 
         [SerializeField]
         private Sprite[] _explosionAnimationFrames;
+
+        private ManagerDialogQuest _managerDialogQuest;
+
+        private void Start()
+        {
+            _managerDialogQuest = FindObjectOfType<ManagerDialogQuest>(true);
+
+            if (_managerDialogQuest.IsCompleted)
+            {
+                Disable();
+            }
+            else
+            {
+                WaitForManagerQuestCompleted().Forget();
+            }
+        }
+
+        private async UniTaskVoid WaitForManagerQuestCompleted()
+        {
+            await UniTask.WaitUntil(() => _managerDialogQuest.IsCompleted);
+
+            _content.SetActive(true);
+        }
+
+        public void Disable()
+        {
+            _content.SetActive(false);
+        }
 
         public async UniTask PlayExplosion()
         {
