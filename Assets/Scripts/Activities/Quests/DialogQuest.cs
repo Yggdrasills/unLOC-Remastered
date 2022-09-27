309c6bdc3e002726e99d3d56d2a2dfd37598384f
Kamalov Khamro <khamrokamalov@gmail.com>
2022-09-27 21:47:14 +0500

    [+] Added lawyer dialog logic

---
+++ Assets/Scripts/Activities/Quests/DialogQuest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Cysharp.Threading.Tasks;
+
+using SevenDays.InkWrapper.Core;
+using SevenDays.InkWrapper.Views.Dialogs;
+using SevenDays.Localization;
+using SevenDays.unLOC.Storage;
+
+using UnityEngine;
+
+namespace SevenDays.unLOC.Activities.Quests
+{
+    public class DialogQuest : QuestBase
+    {
+        protected IStorageRepository Storage;
+
+        [SerializeField]
+        private DialogView _dialogViewBehaviour;
+
+        [SerializeField]
+        private TextAsset _dialogAsset;
+
+        private IDialogView _dialogView;
+        private DialogWrapper _wrapper;
+
+        public void Setup(LocalizationService localization,
+            IStorageRepository storage)
+        {
+            _wrapper = new DialogWrapper(localization);
+            Storage = storage;
+        }
+
+        public void Initialize()
+        {
+            _dialogView = _dialogViewBehaviour;
+
+            Initialized();
+        }
+
+        protected void StartDialog()
+        {
+            _dialogView.ShowAsync().Forget();
+            StartDialogAsync().Forget();
+        }
+
+        protected virtual void Initialized()
+        {
+        }
+
+        protected virtual Dictionary<string, Action> GetDialogTagActions()
+        {
+            return null;
+        }
+
+        protected virtual void DialogCompleted()
+        {
+        }
+
+        private async UniTaskVoid StartDialogAsync()
+        {
+            await _dialogView.ShowAsync();
+
+            var dialog = DialogWrapper.CreateDialog()
+                .SetTextAsset(_dialogAsset)
+                .OnComplete(() =>
+                {
+                    Storage.Save(GetType().FullName, true);
+                    _dialogView.HideAsync().Forget();
+
+                    CompleteQuest();
+
+                    DialogCompleted();
+                });
+
+            var keyActions = GetDialogTagActions();
+
+            if (keyActions != null)
+            {
+                foreach (var item in GetDialogTagActions())
+                {
+                    dialog.SetAction(item.Key, item.Value);
+                }
+            }
+
+            _wrapper.StartDialogueAsync(dialog, _dialogView).Forget();
+        }
+    }
+}
---
+++ Assets/Scripts/Activities/Quests/Lawyer/LawyerDialogQuest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SevenDays.unLOC.Activities.Quests.Lawyer
+{
+    public class LawyerDialogQuest : DialogQuest
+    {
+        [SerializeField]
+        private Button _lawyerButton;
+
+        [SerializeField]
+        private GameObject _content;
+
+        protected override void Initialized()
+        {
+            if (Storage.IsExists(GetType().FullName))
+            {
+                _lawyerButton.gameObject.SetActive(false);
+
+                return;
+            }
+
+            _lawyerButton.onClick.AddListener(OnClick);
+        }
+
+        protected override Dictionary<string, Action> GetDialogTagActions()
+        {
+            return new Dictionary<string, Action>()
+            {
+                {"end1", () => { Debug.Log("Lawyer dialog first end"); }},
+                {"end2", () => { Debug.Log("Lawyer dialog second end"); }},
+            };
+        }
+
+        private void OnClick()
+        {
+            _content.SetActive(false);
+            StartDialog();
+        }
+    }
+}
