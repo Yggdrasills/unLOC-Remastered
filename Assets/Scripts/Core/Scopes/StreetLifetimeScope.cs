e92b0ce38113dab91d33a8c249c7292b71b92c92
Kamalov Khamro <khamrokamalov@gmail.com>
2022-10-27 11:34:43 +0500

    [*] Fixed character creation on street scene

--- Assets/Scripts/Core/Scopes/StreetLifetimeScope.cs
+++ Assets/Scripts/Core/Scopes/StreetLifetimeScope.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-using SevenDays.unLOC.Core.Movement;
-using SevenDays.unLOC.Core.Player;
+﻿using SevenDays.unLOC.Core.Player;
 using SevenDays.unLOC.Utils.Helpers;
 
 using UnityEngine;
 
 using VContainer;
-using VContainer.Unity;
 
 namespace SevenDays.unLOC.Core.Scopes
 {
@@ -22,18 +17,7 @@ namespace SevenDays.unLOC.Core.Scopes
 
         protected override void Configure(IContainerBuilder builder)
         {
-            RegisterMovement(builder);
-        }
-
-        private void RegisterMovement(IContainerBuilder builder)
-        {
-            builder.RegisterInstance(_initializeConfig);
-            IEnumerable<IInputModel> horizontalInput = new[] {new HorizontalInputModel()};
-            builder.RegisterEntryPoint<InputController>().AsSelf().WithParameter(horizontalInput);
-            builder.RegisterEntryPoint<PlayerCreator>()
-                .WithParameter(_camera)
-                .WithParameter(horizontalInput.First())
-                .AsSelf();
+            builder.RegisterCharacter(_camera, _initializeConfig, transform);
         }
     }
 }
